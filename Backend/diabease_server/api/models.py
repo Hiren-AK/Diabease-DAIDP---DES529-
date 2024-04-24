c3143dd5adba12c6f8458fa54282c9ddbbc1c311
Hiren-AK <hirenarvind2013@gmail.com>
2024-04-24 18:37:15 +0530

    check

--- Backend/diabease_server/api/models.py
+++ Backend/diabease_server/api/models.py
@@ -10,9 +10,9 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=50)  # For prototype only, otherwise use Django's user authentication
     age = models.IntegerField()
-    height = models.FloatField()
-    weight = models.FloatField()
+    height = models.FloatField(null=True, blank=True)  # Optional field
+    weight = models.FloatField(null=True, blank=True)  # Optional field
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    diagnosed_duration = models.CharField(max_length=50)
-    dietary_preference = models.CharField(max_length=50)
-    allergies = models.CharField(max_length=100)
+    diagnosed_duration = models.CharField(max_length=50, null=True, blank=True)  # Optional field
+    dietary_preference = models.CharField(max_length=50, null=True, blank=True)  # Optional field
+    allergies = models.CharField(max_length=100, null=True, blank=True)  # Optional field
